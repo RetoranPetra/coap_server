339994c72998b7e13dc6f3c539d3cff072212f81
Luke-Sinclair <lukesinclair27@gmail.com>
2023-04-26 12:41:11 +0100

    Ready to work with motors

--- src/coap_server.c
+++ src/coap_server.c
@@ -4,31 +4,42 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#include <zephyr/kernel.h>
 #include <dk_buttons_and_leds.h>
+#include <openthread/thread.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <math.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/net/openthread.h>
-#include <openthread/thread.h>
+// Channel management
+#include <nrf_802154.h>
+#include <openthread/channel_manager.h>
+#include <openthread/channel_monitor.h>
 
+#include "coap_client_utils.h"
 #include "ot_coap_utils.h"
-//L
-//#include <zephyr/drivers/gpio.h>
-#include <stdlib.h> //For string to float conversion
-//L
-#include <math.h>
+
+// Control modules
+#include "AEDB_9140.h"
+#include "imu.h"
+
 /* 1000 nsec = 1 usec */
 #define MIN_PER 200000
 #define MAX_PER 100000000
 #define full_length_in_steps 3000
 #define pi 3.14159265
+#define step_pin 29
+#define dir_pin 30
+//#define change_dir_pin 11
+//#define print_pin 12
 //#define delta_phi = pi/2/100;
-
-/*Once the client and server are paired. Buttons 1 and 2 on the client are used to set the desired position.
-Button 3 is then pressed to move the motors to that specified position.
-The setting of the position is shown in the log*/
 
 LOG_MODULE_REGISTER(coap_server, CONFIG_COAP_SERVER_LOG_LEVEL);
 
+int yTargetSteps = 0;
+
+const struct device *P0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+//L
 //int position = 0; //Changed from Flag
 float fixed_position = 0; //Placeholder for position
 uint8_t led_toggle = 0;	//To toggle the led GPIO when message received
@@ -46,10 +57,10 @@ static struct k_timer provisioning_timer;
 static void on_light_request(uint8_t command)
 {
 	static uint8_t val;
-
+/*
 	switch (command) {
 
-	case THREAD_STOP_MOTORS: //Code to stop motors
+	case LIGHT_URI_PATH: //Code to stop motors
 		LOG_DBG("Toggling LED\n");
 		dk_set_led(LIGHT_LED, val);
 		val = !val;
@@ -58,6 +69,7 @@ static void on_light_request(uint8_t command)
 	default:
 		break;
 	}
+	*/
 }
 
 static void activate_provisioning(struct k_work *item)
@@ -148,105 +160,135 @@ static void on_generic_request(otChangedFlags flags, struct openthread_context *
 }
 
 static void on_float_request(otChangedFlags flags, struct openthread_context *ot_context, void *user_data) {
-	LOG_DBG("Float request triggered: %d\n", ++count);
-	/*
-	gpio_pin_toggle(P0, 3);
-	dk_set_led(LIGHT_LED, led_toggle); //Toggles LED and pin P0.16	
-	LOG_INF("Float Request event execution!");
-	//Change position and fixed position here to floats and set them accordingly
-	fixed_position = get_float();
-	LOG_DBG("GPIO Toggled!\nOn Float Request float to 2dp: %.2f\nMessage Count: %d\n", fixed_position, ++count);
-	*/
+	yTargetSteps = get_float() *3000;
 }
 
 static struct openthread_state_changed_cb ot_state_chaged_cb = { .state_changed_cb =
 									 on_thread_state_changed };
 
-void main(void)
-{
-	//Need to sleep at start for logs to display correctly.
-	k_msleep(1000);
-	/*uint32_t period = 1U * 1000U * 1000U ; //ms * to_us * to_ns
-	int ySteps = 0;
-	int yTargetSteps = 0;
-	fixed_position = 0.0;
-	int ret;
-	int dir = 1;
-	*/
-	int ret;
 
-	LOG_INF("Start CoAP-server sample");
-
+void main(void) {
+	// Need to sleep at start for logs to display correctly.
+	k_msleep(2000);
+	int ret; 
 	k_timer_init(&led_timer, on_led_timer_expiry, on_led_timer_stop);
 	k_timer_init(&provisioning_timer, on_provisioning_timer_expiry, NULL);
-
+	
 	k_work_init(&provisioning_work, activate_provisioning);
 
 	ret = ot_coap_init(&deactivate_provisionig, &on_light_request, &on_generic_request, &on_float_request);
 	if (ret) {
 		LOG_ERR("Could not initialize OpenThread CoAP");
-		goto end;
 	}
 
 	ret = dk_leds_init();
 	if (ret) {
 		LOG_ERR("Could not initialize leds, err code: %d", ret);
-		goto end;
 	}
 
 	ret = dk_buttons_init(on_button_changed);
 	if (ret) {
 		LOG_ERR("Cannot init buttons (error: %d)", ret);
-		goto end;
 	}
-	gpio_inits();
 
 	openthread_state_changed_cb_register(openthread_get_default_context(), &ot_state_chaged_cb);
 	openthread_start(openthread_get_default_context());
 
-	LOG_INF("Everything initialised correctly\n");
-	//k_sleep(K_NSEC(4000U*1000U*1000U));
+	uint32_t period = 1U * 800U * 1000U ; //ms * to_us * to_ns
+	double per_c = period/1000000000.0;  //ns to s
+	int ySteps = 0;
 
-	/*while (1) {
-		yTargetSteps = fixed_position*full_length_in_steps/100; //Only updates the target position when corresponding message received
-		//Doesn't work when position set once
-		if(yTargetSteps > ySteps){
-			dir = 1;
-			LOG_INF("Moving Forwards");
-		}
-		else if(yTargetSteps < ySteps){
-			LOG_INF("Moving Back");
-			dir = -1;
-		}
-		else{
-			while(ySteps == yTargetSteps){ // Sets target steps when new position set
-				yTargetSteps = fixed_position*full_length_in_steps/100;
-				LOG_INF("Position reached!\nAwaiting Command");
-			}
-		}
- 
-		gpio_pin_set(P0, 3, 1); //Sets step input
+	int dir = 1;
+	double a = -20;
+	double accel = -20;
+	double placeholder = period;
 
-		k_sleep(K_NSEC(period/2U));
-
-		gpio_pin_set(P0, 3, 0); //Sets step input
-
-		k_sleep(K_NSEC(period/2U));
-
-		ySteps = ySteps+dir;
-
-		if(dir == 1)
-			gpio_pin_set(P0, 4, 1); //Sets driver input
-		if(dir == -1)
-			gpio_pin_set(P0, 4, 0); //Sets driver input
-
-		if(period < MIN_PER)
-			period = MIN_PER;
-		if(period > MAX_PER)
-			period = MAX_PER;
-		//k_msleep(1000); //Sleep so inifinite loop does not disturb threads
+	if (!device_is_ready(P0)) {
+		return;
 	}
-	*/
-end:
-	return;
+
+	ret = gpio_pin_configure(P0, step_pin, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+	// ret = gpio_pin_configure(P0, mode2_pin, GPIO_OUTPUT_INACTIVE);
+	// if (ret < 0) {
+	// 	return;
+	// }
+
+	ret = gpio_pin_configure(P0, dir_pin, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+	//gpio_pin_set(P0, mode2_pin, 1);
+
+	printk("Accelerating \n");
+	k_sleep(K_NSEC(8000U*1000U*1000U));
+
+	
+	while (1) {
+		gpio_pin_set(P0, step_pin, 1);
+
+		k_sleep(K_NSEC(period));
+
+		gpio_pin_set(P0, step_pin, 0);
+
+		k_sleep(K_NSEC(period));
+
+		//printk("Period: %u\n", period*2);
+		//printk("yTargetSteps: %i\n", yTargetSteps);
+
+		ySteps = ySteps + dir;
+
+		if(ySteps < yTargetSteps){
+			a = 15*pi*(yTargetSteps-ySteps)/3000;
+			accel = a*dir;
+		}
+		if(ySteps > yTargetSteps){
+			a = -15*pi*(yTargetSteps-ySteps)/3000;
+			accel = a*dir;
+		}
+		while(ySteps == yTargetSteps){
+			printk("Target Reached");
+		}
+		if(dir == 1)
+			gpio_pin_set(P0, dir_pin, 0); //Away from motor
+
+		if(dir == -1)
+			gpio_pin_set(P0, dir_pin, 1); //Towards motor
+    
+    	if( (pi/2/100)*(pi/2/100)/(accel*accel*per_c*per_c*4) + pi/2/100/accel < 0)
+    	{
+			per_c = sqrt((pi/2/100)*(pi/2/100)/(accel*accel*per_c*per_c*4) - pi/2/100/accel) - pi/2/100/(accel*per_c*2);
+			accel = -accel;
+			dir = -dir;
+   		}
+    	else {
+        	if(accel > 0.1) 
+        	{
+    		    per_c = sqrt((pi/2/100)*(pi/2/100)/(accel*accel*per_c*per_c*4) + pi/2/100/accel) - pi/2/100/(accel*per_c*2);
+    		}
+    	else {
+            if(accel < -0.1)
+    		{
+    		        per_c = -sqrt((pi/2/100)*(pi/2/100)/(accel*accel*per_c*per_c*4) + pi/2/100/accel) - pi/2/100/(accel*per_c*2);
+    		}
+		}
+	}
+
+
+	placeholder = per_c*1000000000/2;
+	period = placeholder;
+
+	if(period < MIN_PER){
+		period = MIN_PER;
+		per_c = period/1000000000.0*2;
+	}
+	if(period > MAX_PER){
+		period = MAX_PER;
+		per_c = period/1000000000.0*2;
+		printf("Should have theoretically stopped, per_c = %f\n",per_c);
+	}
 }
